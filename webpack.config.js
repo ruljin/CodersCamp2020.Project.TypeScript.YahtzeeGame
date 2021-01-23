057e4921a55878f0f6c03781dec706d7858ff394
mrozelek <mrozelek@gmail.com>
2021-01-23 20:38:52 +0100

    Setup routing with hot reloading

---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+const path = require('path');
+const NodemonPlugin = require('nodemon-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.ts',
+  mode: 'production',
+  target: 'node',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'index.js'
+  },
+  resolve: {
+    extensions: ['.ts', '.js'],
+  },
+  plugins: [
+    new NodemonPlugin(),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        use: [
+          'ts-loader',
+        ]
+      }
+    ]
+  }
+}
