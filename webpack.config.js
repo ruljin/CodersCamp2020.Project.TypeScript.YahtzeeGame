1a74f6f7e1575614517e1e2beff4d51903b94dc3
mrozelek <mrozelek@gmail.com>
2021-01-24 14:49:01 +0100

    Add webpack style loaders, configure typescript, optimalize build

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,12 @@
 const path = require('path');
 const NodemonPlugin = require('nodemon-webpack-plugin');
+const nodeExternals = require('webpack-node-externals');
 
 module.exports = {
   entry: './src/index.ts',
   mode: 'production',
   target: 'node',
+  externals: [ nodeExternals() ],
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'index.js'
@@ -21,6 +23,26 @@ module.exports = {
         test: /\.ts$/,
         use: [
           'ts-loader',
+        ]
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource'
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          'style-loader',
+          'css-loader',
+          {
+            loader: 'sass-loader',
+            options: {
+              sourceMap: true,
+              sassOptions: {
+                outputStyle: 'compressed'
+              }
+            }
+          }
         ]
       }
     ]
