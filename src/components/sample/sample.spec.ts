3ce0ebcd4f276363f7e7e9c27e194e1b9c919884
mrozelek <mrozelek@gmail.com>
2021-01-25 22:27:55 +0100

    Fix sample test

--- src/components/sample/sample.spec.ts
+++ src/components/sample/sample.spec.ts
@@ -1,5 +1,5 @@
 import { SampleComponent } from './sample';
 
 test('Test rendering', () => {
-  expect((new SampleComponent()).render()).toBe('<p class="my-p">sample text</p>');
+  expect((new SampleComponent()).render()).toBe('<p class="my-p" id="myP">sample text</p>');
 });
