ce594dd2a7ca4be31ce783b12322e0fb38de5316
Weronika Brzęczkowska <brzeczkowska.w@gmail.com>
2021-01-29 16:36:50 +0100

    lint corrections added

--- src/components/intro/intro.spec.ts
+++ src/components/intro/intro.spec.ts
@@ -3,17 +3,17 @@ import { createElementFromString } from '../../common/WebComponent';
 
 
 test('Check rendering intro text', () => {
-    const intro = createElementFromString(`<main>
+  const intro = createElementFromString(`<main>
         <div class="container">
         Yahtzee is a dice game based on Poker.<br>
         You can choose to play against computer or with your friends.<br>
         Check game rules and set your settings before the game.<br>
         Your best scores will be saved.<br>
-        Enjoy playing Yahtzee! 
+        Enjoy playing Yahtzee!
         </div>
       </main>`);
-  
-    const introComponent = new IntroComponent();
-  
-    expect(introComponent.render()).toStrictEqual(intro);
-  });
+
+  const introComponent = new IntroComponent();
+
+  expect(introComponent.render()).toStrictEqual(intro);
+});
--- src/components/intro/intro.ts
+++ src/components/intro/intro.ts
@@ -3,21 +3,21 @@ import WebComponent, { createElementFromString } from '../../common/WebComponent
 
 
 class IntroComponent implements WebComponent {
-    render(): Element {
-      return createElementFromString(`<main>
+  render(): Element {
+    return createElementFromString(`<main>
         <div class="container">
         Yahtzee is a dice game based on Poker.<br>
         You can choose to play against computer or with your friends.<br>
         Check game rules and set your settings before the game.<br>
         Your best scores will be saved.<br>
-        Enjoy playing Yahtzee! 
+        Enjoy playing Yahtzee!
         </div>
       </main>`);
-    }
-  
-    setup(): void {
-      return;
-    }
   }
-  
-  export default IntroComponent;
+
+  setup(): void {
+    return;
+  }
+}
+
+export default IntroComponent;
