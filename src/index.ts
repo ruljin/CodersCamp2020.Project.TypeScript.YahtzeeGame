138de1d67d7b9e241e198debcce25e5625415a76
Kamila Grusza <kamila.grusza@tlen.pl>
2021-01-29 16:52:24 +0100

    fix index.ts

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,6 @@ import SampleComponent from './components/sample/sample';
 import Router from './common/Router';
 import { createElementFromString } from './common/WebComponent';
 import LogoComponent from './components/logo/logo';
-import LabelsComponent from './components/labels/labels';
 
 const router = new Router(document.querySelector('#root')!);
 
@@ -17,26 +16,6 @@ function routePathsHandler() {
 
     const logo = new LogoComponent();
     router.renderComponent(logo.render());
-
-    const label1 = new LabelsComponent();
-    label1.settingsPlayers();
-    router.renderComponent(label1.render());
-    label1.setup();
-
-    const label2 = new LabelsComponent();
-    label2.settingsStyle();
-    router.renderComponent(label2.render());
-    label2.setup();
-
-    const label3 = new LabelsComponent();
-    label3.boardWhoPlays('Maciej');
-    router.renderComponent(label3.render());
-    label3.setup();
-
-    const label4 = new LabelsComponent();
-    label4.boardChooseCategory();
-    router.renderComponent(label4.render());
-    label4.setup();
 
     const sampleComponent = new SampleComponent();
     router.renderComponent(sampleComponent.render());
