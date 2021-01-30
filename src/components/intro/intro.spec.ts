a63417c04743a3c9a9aeffb8f5efe668a32d53fc
mrozelek <mrozelek@gmail.com>
2021-01-30 18:14:01 +0100

    Remove unnecessary div

--- src/components/intro/intro.spec.ts
+++ src/components/intro/intro.spec.ts
@@ -3,14 +3,13 @@ import { createElementFromString } from '../../common/WebComponent';
 
 
 test('Check rendering intro text', () => {
-  const intro = createElementFromString(`<section>
-        <div class="container">
+  const intro = createElementFromString(`
+      <section class="container">
         Yahtzee is a dice game based on Poker.<br>
         You can choose to play against computer or with your friends.<br>
         Check game rules and set your settings before the game.<br>
         Your best scores will be saved.<br>
         Enjoy playing Yahtzee!
-        </div>
       </section>`);
 
   const introComponent = new IntroComponent();
--- src/components/intro/intro.ts
+++ src/components/intro/intro.ts
@@ -4,14 +4,13 @@ import WebComponent, { createElementFromString } from '../../common/WebComponent
 
 class IntroComponent implements WebComponent {
   render(): Element {
-    return createElementFromString(`<section>
-        <div class="container">
+    return createElementFromString(`
+      <section class="container">
         Yahtzee is a dice game based on Poker.<br>
         You can choose to play against computer or with your friends.<br>
         Check game rules and set your settings before the game.<br>
         Your best scores will be saved.<br>
         Enjoy playing Yahtzee!
-        </div>
       </section>`);
   }
 
