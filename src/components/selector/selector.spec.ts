1a59811573bb8fcbdbe53066cb34e28acd1f9104
mrozelek <mrozelek@gmail.com>
2021-01-30 22:28:55 +0100

    Fix formatting and linter errors

--- src/components/selector/selector.spec.ts
+++ src/components/selector/selector.spec.ts
@@ -4,15 +4,15 @@ import { createElementFromString } from '../../common/WebComponent';
 test('component rendering test', () => {
   const dropDown: HTMLSelectElement = document.createElement('select');
   const selector = createElementFromString(
-    `<div class ="selector"> 
-          <select>
-            <option selected="selected" disabled hidden>Select oponent: </option>
-            <option value="0">computer/easy</option>
-            <option value="1">computer/medium</option>
-            <option value="2">computer/hard</option>
-            <option value="3">player</option>
-          </select>
-        </div>`);
+    `<div class="selector">
+        <select>
+          <option selected disabled hidden>Select oponent: </option>
+          <option value="0">computer/easy</option>
+          <option value="1">computer/medium</option>
+          <option value="2">computer/hard</option>
+          <option value="3">player</option>
+        </select>
+      </div>`);
 
   const selectorComp = new SelectorComponent(dropDown);
 
--- src/components/selector/selector.ts
+++ src/components/selector/selector.ts
@@ -2,41 +2,43 @@ import './selector.scss';
 import WebComponent, { createElementFromString } from '../../common/WebComponent';
 
 class SelectorComponent implements WebComponent {
-    dropdownMenu: HTMLSelectElement;
-    options: HTMLOptionsCollection;
+  dropdownMenu: HTMLSelectElement;
+  options: HTMLOptionsCollection;
 
-    render(): Element {
-      return createElementFromString(
-        `<div class ="selector"> 
-          <select>
-            <option selected="selected" disabled hidden>Select oponent: </option>
-            <option value="0">computer/easy</option>
-            <option value="1">computer/medium</option>
-            <option value="2">computer/hard</option>
-            <option value="3">player</option>
-          </select>
-        </div>`);
-    }
-    setup(): void {
-      return;
-    }
+  constructor(dropdown: HTMLSelectElement) {
+    this.dropdownMenu = dropdown;
+    this.options = dropdown.options;
+  }
 
-    constructor(dropdown: HTMLSelectElement) {
-      this.dropdownMenu = dropdown;
-      this.options = dropdown.options;
-    }
+  render(): Element {
+    return createElementFromString(
+      `<div class="selector">
+        <select>
+          <option selected disabled hidden>Select oponent: </option>
+          <option value="0">computer/easy</option>
+          <option value="1">computer/medium</option>
+          <option value="2">computer/hard</option>
+          <option value="3">player</option>
+        </select>
+      </div>`);
+  }
 
-    onChange(): void {
-      document.location.href = this.options[this.options.selectedIndex].value;
-    }
+  setup(): void {
+    return;
+  }
 
-    onLoad = () => {
-      const select1: HTMLSelectElement = document.getElementById('select')! as HTMLSelectElement;
-      const ddm1: SelectorComponent = new SelectorComponent(select1);
-      select1.onchange = () => {
-        ddm1.onChange();
-      };
-    }
+  onChange(): void {
+    document.location.href = this.options[this.options.selectedIndex].value;
+  }
+
+  onLoad(): void {
+    const select1: HTMLSelectElement = document.getElementById('select')! as HTMLSelectElement;
+    const ddm1: SelectorComponent = new SelectorComponent(select1);
+
+    select1.onchange = () => {
+      ddm1.onChange();
+    };
+  }
 }
 
 export default SelectorComponent;
