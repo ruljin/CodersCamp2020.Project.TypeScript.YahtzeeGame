2e4a33fe5448a8d14c40d98a1bc2a1e0da25f341
mrozelek <mrozelek@gmail.com>
2021-01-30 18:27:24 +0100

    Remove unnecessary div in authors component

--- src/components/authors/authors.spec.ts
+++ src/components/authors/authors.spec.ts
@@ -3,17 +3,16 @@ import { createElementFromString } from '../../common/WebComponent';
 
 
 test('Check rendering authors text', () => {
-  const intro = createElementFromString(`<section>
-        <div class="container">
+  const intro = createElementFromString(`
+        <section class="container">
         <a class="link" href="https://github.com/kami3la" target="_blank">Kamila Grusza</a><br>
         <a class="link" href="https://github.com/KonradMierzejewski" target="_blank">Konrad Mierzejewski</a><br>
         <a class="link" href="https://github.com/brzeczkowskaw" target="_blank">Weronika Brzęczkowska-Kuzianik</a><br>
         <a class="link" href="https://github.com/adax10" target="_blank">Adrianna Krupa</a><br>
-        <a class="link" href="https://github.com/Suegro24 target="_blank">Dominik Puchała</a><br>
+        <a class="link" href="https://github.com/Suegro24" target="_blank">Dominik Puchała</a><br>
         <a class="link" href="https://github.com/Mrozelek" target="_blank">Jędrzej Ratajczak</a><br>
         <a class="link" href="https://github.com/ruljin" target="_blank">Filip Kuca - support</a>
-        </div>
-      </section>`);
+        </section>`);
 
   const authorsComponent = new AuthorsComponent();
 
--- src/components/authors/authors.ts
+++ src/components/authors/authors.ts
@@ -4,8 +4,8 @@ import WebComponent, { createElementFromString } from '../../common/WebComponent
 
 class AuthorsComponent implements WebComponent {
   render(): Element {
-    return createElementFromString(`<section>
-        <div class="container">
+    return createElementFromString(`
+        <section class="container">
         <a class="link" href="https://github.com/kami3la" target="_blank">Kamila Grusza</a><br>
         <a class="link" href="https://github.com/KonradMierzejewski" target="_blank">Konrad Mierzejewski</a><br>
         <a class="link" href="https://github.com/brzeczkowskaw" target="_blank">Weronika Brzęczkowska-Kuzianik</a><br>
@@ -13,8 +13,7 @@ class AuthorsComponent implements WebComponent {
         <a class="link" href="https://github.com/Suegro24" target="_blank">Dominik Puchała</a><br>
         <a class="link" href="https://github.com/Mrozelek" target="_blank">Jędrzej Ratajczak</a><br>
         <a class="link" href="https://github.com/ruljin" target="_blank">Filip Kuca - support</a>
-        </div>
-      </section>`);
+        </section>`);
   }
 
   setup(): void {
