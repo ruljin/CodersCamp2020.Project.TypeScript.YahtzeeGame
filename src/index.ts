46cb8d77172491e2d07edf52a7f1d5e9768e715c
Kamila Grusza <kamila.grusza@tlen.pl>
2021-01-30 19:32:43 +0100

    remove unnecessary code

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,6 @@ import SampleComponent from './components/sample/sample';
 import Router from './common/Router';
 import { createElementFromString } from './common/WebComponent';
 import DiceBackground, { DiceTypes } from './components/dice-background/dice-background';
-import LabelComponent from './components/label/label';
 
 const router = new Router(document.querySelector('#root')!);
 function routePathsHandler() {
@@ -20,15 +19,6 @@ function routePathsHandler() {
     const sampleComponent = new SampleComponent();
     router.renderComponent(sampleComponent.render());
     sampleComponent.setup();
-
-    const x = new LabelComponent('Ania plays!');
-    router.renderComponent(x.render());
-    x.setup();
-
-    const y = new LabelComponent('style');
-    y.alternateAppearance();
-    router.renderComponent(y.render());
-    y.setup();
 
     router.renderComponent(createElementFromString('<a href="#">Go back</a>'));
   }
