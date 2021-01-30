b27c2696f064dd4abf83b727c79571a4428a617d
mrozelek <mrozelek@gmail.com>
2021-01-30 18:19:56 +0100

    Fix tests for dice-background component

--- src/components/dice-background/dice-background.spec.ts
+++ src/components/dice-background/dice-background.spec.ts
@@ -9,26 +9,26 @@ test('test rendering default dice', () => {
   const diceBackground = new DiceBackground(DiceTypes.BG);
 
   expect(diceBackground.render())
-    .toStrictEqual(createElementFromString(`<img src="${Dice}" alt="Background image of dice">`));
+    .toStrictEqual(createElementFromString(`<img src="${Dice}" class="background-dice background-dice--default" alt="Background image of dice">`));
 });
 
 test('test rendering alternative dice', () => {
   const diceBackground = new DiceBackground(DiceTypes.BG_ALT);
 
   expect(diceBackground.render())
-    .toStrictEqual(createElementFromString(`<img src="${DiceAlt}" alt="Background image of dice">`));
+    .toStrictEqual(createElementFromString(`<img src="${DiceAlt}" class="background-dice background-dice--alt" alt="Background image of dice">`));
 });
 
 test('test rendering mobile dice', () => {
   const diceBackground = new DiceBackground(DiceTypes.BG_MOB);
 
   expect(diceBackground.render())
-    .toStrictEqual(createElementFromString(`<img src="${DiceMob}" alt="Background image of dice">`));
+    .toStrictEqual(createElementFromString(`<img src="${DiceMob}" class="background-dice background-dice--mob" alt="Background image of dice">`));
 });
 
 test('test rendering alternative mobile dice', () => {
   const diceBackground = new DiceBackground(DiceTypes.BG_MOB_ALT);
 
   expect(diceBackground.render())
-    .toStrictEqual(createElementFromString(`<img src="${DiceMobAlt}" alt="Background image of dice">`));
+    .toStrictEqual(createElementFromString(`<img src="${DiceMobAlt}" class="background-dice background-dice--mob-alt" alt="Background image of dice">`));
 });
