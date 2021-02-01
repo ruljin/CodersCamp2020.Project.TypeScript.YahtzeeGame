be3e3578f92f544c2b843b9ea8bc4d878c3473e4
Konrad <k.mierzejewski07@gmail.com>
2021-02-01 13:36:57 +0100

    Added selector component with default styling

---
+++ src/components/selectorAlt/selectorAlt.ts
@@ -0,0 +1,30 @@
+import './selectorAlt.scss';
+import WebComponent, { createElementFromString } from '../../common/WebComponent';
+
+class SelectorComponent implements WebComponent {
+  private selector: HTMLElement = document.createElement('null');
+
+  constructor(private list: string[], private width: number) {}
+
+  render(): Element {
+    this.selector = createElementFromString(`
+       <select class="select">
+        <option class="opt" value="default"></option>
+       </select>`) as HTMLElement;
+    this.selector.style.width = `${this.width}rem`;
+    return this.selector as Element;
+  }
+
+  setup(): void {
+    const arr = this.list;
+    for (let i =0; i<= arr.length - 1; i++) {
+      const option = document.createElement('option'),
+        txt = document.createTextNode(arr[i]);
+      option.appendChild(txt);
+      option.setAttribute('value', arr[i]);
+      this.selector.insertBefore(option, this.selector.lastChild);
+    }
+  }
+}
+
+export default SelectorComponent;
--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,9 @@ import SampleComponent from './components/sample/sample';
 import Router from './common/Router';
 import { createElementFromString } from './common/WebComponent';
 import DiceBackground, { DiceTypes } from './components/dice-background/dice-background';
+import SelectorComponent from './components/selectorAlt/selectorAlt';
+
+const list: string[] = ['one', 'two', 'three'];
 
 const router = new Router(document.querySelector('#root')!);
 function routePathsHandler() {
@@ -19,6 +22,10 @@ function routePathsHandler() {
     const sampleComponent = new SampleComponent();
     router.renderComponent(sampleComponent.render());
     sampleComponent.setup();
+
+    const selectorComponent = new SelectorComponent(list, 5);
+    router.renderComponent(selectorComponent.render());
+    selectorComponent.setup();
 
     router.renderComponent(createElementFromString('<a href="#">Go back</a>'));
   }
